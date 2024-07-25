200e65aec0d1dc161eae8a5a71709e5050d98a23
e4y6 <tchehy9e@gmaile.com>
2024-07-25 23:49:53 +0300

    modified:   js/08-tr-4kyu.js

--- js/08-tr-4kyu.js
+++ js/08-tr-4kyu.js
@@ -3910,5 +3910,45 @@ Hence the difference between square of the sum of the first ten natural numbers 
     }
     return sum ** 2 - squereSum;
   }
-  console.log(differenceOfSquares(5));
+  // console.log(differenceOfSquares(5));
+}
+
+{
+  /** 6 kyu  Rectangle into Squares
+   * The drawing below gives an idea of how to cut a given "true" rectangle into squares ("true" rectangle meaning that the two dimensions are different).
+alternative text
+Can you translate this drawing into an algorithm?
+You will be given two dimensions
+    a positive integer length
+    a positive integer width
+You will return a collection or a string (depending on the language; Shell bash, PowerShell, Pascal and Fortran return a string) with the size of each of the squares.
+Examples in general form:
+(depending on the language)
+  sqInRect(5, 3) should return [3, 2, 1, 1]
+  sqInRect(3, 5) should return [3, 2, 1, 1]
+  You can see examples for your language in **"SAMPLE TESTS".**
+Notes:
+    lng == wdth as a starting case would be an entirely different problem and the drawing is planned to be interpreted with lng != wdth. (See kata, Square into Squares. Protect trees! http://www.codewars.com/kata/54eb33e5bc1a25440d000891 for this problem).
+    When the initial parameters are so that lng == wdth, the solution [lng] would be the most obvious but not in the spirit of this kata so, in that case, return None/nil/null/Nothing or return {} with C++, Array() with Scala, [] with Perl, Raku.
+    In that case the returned structure of C will have its sz component equal to 0.             */
+
+  function sqInRect(lng, wdth) {
+    if (lng === wdth) return null;
+    let sq = lng * wdth;
+    const arr = [];
+
+    while (sq >= 1) {
+      let max = Math.max(lng, wdth);
+      let min = Math.min(lng, wdth);
+      lng = max;
+      wdth = min;
+      arr.push(wdth);
+      sq -= wdth ** 2;
+      lng -= wdth;
+    }
+    return arr;
+  }
+  // console.log(sqInRect(20, 14));
+  // console.log(sqInRect(5, 3));
+  // console.log(sqInRect(3, 5));
 }
