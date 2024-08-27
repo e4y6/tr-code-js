a3858215cd41de1fa2b3dad955a18b088d32ae61
e4y6 <tchehy9e@gmaile.com>
2024-08-27 20:14:38 +0300

    modified:   js/09-tr-3kyu.js

--- js/09-tr-3kyu.js
+++ js/09-tr-3kyu.js
@@ -23,3 +23,12 @@ You can assume that the number is not negative.            */
   }
   // console.log(reverseBits(417));
 }
+{
+  /** 7 kyu  Holiday III - Fire on the boat  
+   * Enjoying your holiday, you head out on a scuba diving trip!
+Disaster!! The boat has caught fire!!
+You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.    */
+
+  const fireFight = s => s.replace(/\bFire\b/g, '~~');
+  // console.log(fireFight('Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast' ))
+}
