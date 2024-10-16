eb273a0e96d91a470c96a786bbdfeb46f3a29ea7
e4y6 <tchehy9@gmail.com>
2024-10-17 00:38:46 +0300

    modified:   js/09-tr-3kyu.js

--- js/09-tr-3kyu.js
+++ js/09-tr-3kyu.js
@@ -124,13 +124,32 @@ The input can be modified   */
 }
 
 {
-  function last(...args) {
-    console.log(args);
-    return args.length > 1
+  /** 7 kyu  Last
+  Find the last element of the given argument(s). If a single argument is passed and is a list/array or a string, return its last element. It is guaranteed that there will be at least one argument and that single-argument arrays/lists/strings will not be empty.
+  
+  Examples
+  last(5)               ==> 5
+  last([1, 2, 3, 4])    ==>  4
+  last("xyz")           ==> "z"
+  last(1, 2, 3, 4)      ==>  4
+  last([1, 2], [3, 4])  ==>  [3, 4]
+  last([[1, 2], [3, 4]])  ==>  [3, 4] */
+
+  // function last(...args) {
+  //   console.log(args);
+  //   return args.length > 1
+  //     ? args.slice(-1)[0]
+  //     : args[0].length > 1
+  //     ? args[0].slice(-1)[0]
+  //     : args[0][0] ?? args[0];
+  // }
+
+  const last = (...args) =>
+    args.length > 1
       ? args.slice(-1)[0]
       : args[0].length > 1
       ? args[0].slice(-1)[0]
       : args[0][0] ?? args[0];
-  }
+
   console.log(last([7]));
 }
