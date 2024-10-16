1ffff618d3d61a44b2f7d1e23e36d81a20032889
e4y6 <tchehy9@gmail.com>
2024-10-16 19:18:03 +0300

    modified:   js/09-tr-3kyu.js

--- js/09-tr-3kyu.js
+++ js/09-tr-3kyu.js
@@ -164,5 +164,23 @@ A class/static method greetExtraTerrestrials/GreetExtraTerrestrials that accepts
   }
 
   const john = new Person();
-  console.log(john);
+  // console.log(john);
+}
+
+{
+  /** 7 kyu  Two numbers are positive
+  Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of the three integers are positive numbers (greater than zero), and False - otherwise.
+  
+  Examples:
+  twoArePositive(2, 4, -3) == true
+  twoArePositive(-4, 6, 8) == true
+  twoArePositive(4, -6, 9) == true
+  twoArePositive(-4, 6, 0) == false
+  twoArePositive(4, 6, 10) == false
+  twoArePositive(-14, -3, -4) == false */
+
+  function twoArePositive(a, b, c) {
+    return [a, b, c].filter((e) => e > 0).length === 2;
+  }
+  console.log(twoArePositive(0, 1, 2));
 }
