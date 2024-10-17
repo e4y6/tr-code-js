8731667cee175efed6fda197b99754f6ef74e3b6
e4y6 <tchehy9@gmail.com>
2024-10-17 22:23:28 +0300

    modified:   js/09-tr-3kyu.js

--- js/09-tr-3kyu.js
+++ js/09-tr-3kyu.js
@@ -154,5 +154,19 @@ The input can be modified   */
   const last = (a, ...args) =>
     args.length ? args.slice(-1)[0] : a.length > 1 ? a.slice(-1)[0] : a[0] ?? a;
 
-  console.log(last([7]));
+  // console.log(last([7]));
+}
+
+{ /**  
+7 kyu  Multiply Word in String
+    You are to write a function that takes a string as its first parameter. This string will be a string of words.
+    You are expected to then use the second parameter, which will be an integer, to find the corresponding word in the given string. The first word would be represented by 0.
+    Once you have the located string you are finally going to multiply by it the third provided parameter, which will also be an integer. You are additionally required to add a hyphen in between each word.
+Example
+modifyMultiply ("This is a string", 3, 5)   */
+
+function modifyMultiply (str,loc,num) {
+return Array(num).fill(str.split(' ')[loc]).join('-')
+} 
+    console.log(modifyMultiply("Is sloppiness in code caused by ignorance or apathy? I don't know and I don't care.",6 ,8))
 }
