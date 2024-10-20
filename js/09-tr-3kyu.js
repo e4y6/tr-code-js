70d4181a785da418ccc60e2a7a5280191dee936d
e4y6 <tchehy9@gmail.com>
2024-10-20 18:35:54 +0300

    modified:   js/09-tr-3kyu.js

--- js/09-tr-3kyu.js
+++ js/09-tr-3kyu.js
@@ -231,5 +231,38 @@ result = [6, 3, 7, 5, 4, 7]   */
     // d <= 0 ? [] : [...(n + "").slice(-d)].map((e) => +e);
     d > 0 ? [...(n + "").slice(-d)].map(Number) : [];
 
-  console.log(lastDigit(34625647867585, 10));
+  // console.log(lastDigit(34625647867585, 10));
+}
+
+{
+  /**   7 kyu  Caffeine Script
+  Complete the function which takes a non-zero integer as its argument.
+  
+  If the integer is divisible by 3, return the string "Java".
+  
+  If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+  
+  If one of the condition above is true and the integer is even, add "Script" to the end of the string.
+  
+  If none of the condition is true, return the string "mocha_missing!"
+  Examples
+  
+  1   -->  "mocha_missing!"
+  3   -->  "Java"
+  6   -->  "JavaScript"
+  12  -->  "CoffeeScript"   */
+
+  const caffeineBuzz = (n) => {
+    if (!(n % 12)) {
+      return "CoffeeScript";
+    }
+    if (!(n % 6)) {
+      return "JavaScript";
+    }
+    if (!(n % 3)) {
+      return "Java";
+    }
+    return "mocha_missing!";
+  };
+  // console.log(caffeineBuzz(112));
 }
