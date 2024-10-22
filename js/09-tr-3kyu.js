9d8f85b4525494246b470cd355210949fb02adc4
e4y6 <tchehy9@gmail.com>
2024-10-22 18:26:59 +0300

    modified:   js/09-tr-3kyu.js

--- js/09-tr-3kyu.js
+++ js/09-tr-3kyu.js
@@ -354,5 +354,41 @@ Notes:
 
   const alienLanguage = (str) =>
     str.toUpperCase().replace(/\w\b/g, (e) => e.toLowerCase());
-  console.log(alienLanguage("My name is John"));
+  // console.log(alienLanguage("My name is John"));
+}
+
+{
+  /** 7 kyu  Cat Years, Dog Years (2)
+  I have a cat and a dog which I got as kitten / puppy.
+
+I forget when that was, but I do know their current ages as catYears and dogYears.
+
+Find how long I have owned each of my pets and return as a list [ownedCat, ownedDog]
+
+NOTES:
+
+    Results are truncated whole numbers of "human" years
+
+Cat Years
+
+    15 cat years for first year
+    +9 cat years for second year
+    +4 cat years for each year after that
+
+Dog Years
+
+    15 dog years for first year
+    +9 dog years for second year
+    +5 dog years for each year after that    */
+
+  const ownedCatAndDog = function (catYears, dogYears) {
+    const animal = (age, rate) => {
+      if (age < 15) return 0;
+      if (age < 24) return 1;
+      return (2 + (age - 24) / rate) ^ 0;
+    };
+    return [animal(catYears, 4), animal(dogYears, 5)];
+  };
+
+  console.log(ownedCatAndDog(56, 64));
 }
